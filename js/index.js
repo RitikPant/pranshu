2c20f0ead0619829a7fbfba9128e1e1a4b9b6db5
Ritik Pant <pantritik2203@gmail.com>
2023-12-30 22:01:22 +0530

    personal information

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function copy(that) {
+
+    showCopiedToast();
+    var inp = document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value = that.textContent
+    inp.select();
+    document.execCommand('copy', false);
+    inp.remove();
+}
+
+function showCopiedToast() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
+}
